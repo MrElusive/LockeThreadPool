77b17687d8fe33b0af282f82ec46418345e0c9ba
sdai <sierradai@gmail.com>
2015-08-03 20:11:23 -0500

    Update, unfinished

--- LockeThreadPool/src/main/java/edu/utexas/locke/LockeThreadPool.java
+++ LockeThreadPool/src/main/java/edu/utexas/locke/LockeThreadPool.java
@@ -1,7 +1,11 @@
  package main.java.edu.utexas.locke;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadPoolExecutor;
 
 // Sierra
 public class LockeThreadPool {
@@ -11,32 +15,28 @@ public class LockeThreadPool {
 	// ComputationTracker.initialize(numLockeThreads);
 	// This will allow us to keep track of when all LockeThreads
 	// have completed.
-	
-	//To create multiple processes and their associated serial machines, a program instantiates 
-	// an object of type HoodParMach Ñ a Òparallel machine.Ó 
-	// The HoodParMach constructor takes an integer argument P . 
-	// It creates P serial machines, and it forks P processes. 
-	// Initially, the P processes just park on a condition variable. 
-	// After creating the parallel machine and one or more threads, 
-	// the program calls the HoodParMach runScheduler method. 
-	// The P processes are released from the condition variable, 
-	// and they begin executing the scheduling loop, as described in the next section.
-	// When the scheduling loop terminates, they repark and runScheduler returns.
-	// Member Variables
-	//  startExecution (condition variable)
-	// Methods
-	//void runScheduler()
 
 	Set<LockeProcess> processes;
+	private ThreadPoolExecutor threadPoolExecutor;
+	private Semaphore startSemaphore;
+	
+	@SuppressWarnings("unused")
+	private ExecutorService threadPool;
 
 	public LockeThreadPool(int numProcesses) {
-		processes = new HashSet<LockeProcess>();
+		this.startSemaphore = new Semaphore(numProcesses, true);
+		threadPool = Executors.newFixedThreadPool(numProcesses);
+		final CountDownLatch cdl = new CountDownLatch(numProcesses);
+		
+		//processes = new HashSet<LockeProcess>();
 		for (int i = 0; i < numProcesses; i++) {
 			LockeProcess process = new LockeProcess(startSemaphore);
 			process.start();
 			processes.add(process);
 		}
 	}
+	
+	
 
 	public void submit(Collection<LockeThread> threads) {
 		for (LockeThread thread : threads) {
@@ -46,11 +46,35 @@ public class LockeThreadPool {
 		startSemaphore.release(processes.size());
 	}
 
+	
+	
+	
 	public <T> T invoke(LockeTask<T> task) {
+		
+		
+		try {
+			startSemaphore.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		CountDownLatch count;
+		
 		return getRandomProcess().invokeTask(task);
+		
 	}
+	
+	
+
 
 	public static LockeProcess getRandomProcess() {
 		return null;
 	}
+	
+	
+	
+	
+	
+	
 }
